1df96803c341c0c924769f26a6a7a4da309fb33e
Ricky <ricky@Development.local>
2017-09-14 16:53:57 +0200

    type: feat
    body: Added CategoryView component, not finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,17 @@ import { Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './header/header'
+import CategoryView from './category-view/CategoryView'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" render={() => (
+        <Route exact path="/" render={() => (
           <Header />
+        )} />
+        <Route path="/category/:category" render={() => (
+          <CategoryView />
         )} />
       </div>
     );
---
+++ frontend/src/category-view/CategoryView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CategoryView extends Component {
+  render() {
+    return (
+      <div>Test</div>
+    )
+  }
+}
+
+export default CategoryView
